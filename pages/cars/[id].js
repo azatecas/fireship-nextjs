b9b1d0d846c880a5142efebac2c62f1842eda3b0
azatecas <luisp237@gmail.com>
2021-09-04 19:26:36 -0400

    getServerSidePops() getStaticProps() and getStaticPaths() functions

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,8 +1,60 @@
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-export default function Cars() {
+export default function Cars({ car }) {
     const router = useRouter();
     const { id } = router.query;
 
-    return <h1>Hello {id}</h1>
+    return (
+        <>
+            <Head>
+                <title>{car.color}</title>
+            </Head>
+            <h1>Hello {id}</h1>
+            <img src={car.image} style={{"width":"400px"}}/>
+        </>
+        )
+}
+
+
+/**
+ * This function does the same as getStaticProps but it regenerates components based on request
+ */
+// export async function getServerSideProps() {
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
+
+//     return {
+//         props: { car: data},
+//     }
+// }
+
+//tells nextjs to prerender page
+export async function getStaticProps({ params }){
+
+    const req = await fetch(`http://localhost:3000/${params.id}.json`);
+    const data = await req.json();
+
+    return {
+        props: { car: data},
+    }
+}
+
+/**
+ * Tells NextJS which dynamic pages to render
+ * 
+ * it can fetch from an api as well and it's job is to return an array of paths
+ */
+export async function getStaticPaths(){
+    const req = await fetch('http://localhost:3000/cars.json');
+    const data = await req.json();
+
+    const paths = data.map(car => {
+        return { params: { id: car} }
+    })
+
+    return {
+        paths,
+        fallback: false
+    }
 }
